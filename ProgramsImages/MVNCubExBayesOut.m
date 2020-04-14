ba1e06791ed1c427e92ee1360a281db1fbcc2303
Fred J Hickernell <hickernell@iit.edu>
2020-04-14 00:01:37 -0500

    Revising talk and example

--- ProgramsImages/MVNCubExBayesOut.m
+++ ProgramsImages/MVNCubExBayesOut.m
@@ -6,19 +6,23 @@ function MVNCubExBayesOut(dataFileName)
 
 gail.InitializeDisplay
 
-GAILPATH = GAILstart(0);
-dirpath = strcat([GAILPATH,'OutputFiles',filesep], 'Paper_cubBayesLattice_g');
-
+% GAILPATH = GAILstart(0);
+% dirpath = strcat([GAILPATH,'OutputFiles',filesep], 'Paper_cubBayesLattice_g');
+dirpath = '.';
 load(dataFileName);
 %% Output
 fid = fopen([dirpath, filesep, 'MVNBayesOut.txt'], 'wt');
-for i = 1:length(avgAbsErrMC)
-  if i == 1
-  fprintf(fid,'& \\multicolumn{6}{c}{\\Sigma = \\bf{I}_d, \\ \\bf{b}=-\\bf{a}=(3.5,\\dots,3.5) } \\\\ \\hline \n');
-  else
-  fprintf(fid,'& \\multicolumn{6}{c}{\\Sigma = 0.4 \\bf{I}_d + 0.6\\bf{1}\\bf{1}^T , \\ \\bf{a}=(-\\infty,\\dots,-\\infty), \\ \\bf{b}=\\sqrt{d}(U_1,\\dots,U_d) } \\\\ \\hline \n');
-  end
-  fprintf(fid,' \\text{Method} & \\text{MC} & \\text{Lattice} & \\text{Sobol} & \\text{BayesLat} & \\text{BayesSobol}  \\\\\n');
+
+returnCode = '\n';
+fprintf(fid,'\\[ \n \\begin{array}{rccccc} \n \\toprule \n');
+istart = 2; 
+for i = istart:length(avgAbsErrMC)
+   if i == 1
+      fprintf(fid,'\\multicolumn{6}{c}{\\varepsilon = %4.3f, \\ d = %1.0f,\\ \\mSigma = \\mI, \\ \\vb=-\\va=(3.5,\\dots,3.5) } \\\\ \\midrule \n', abstol, d);
+   else
+	   fprintf(fid,'\\multicolumn{6}{c}{\\varepsilon = %4.3f, \\ d = %1.0f,\\ \\mSigma = 0.4 \\mI, + 0.6\\vone\\vone^T , \\ \\va=(-\\infty,\\dots,-\\infty), \\ \\vb\\sim \\sqrt{d}\\cu[0,1]^d } \\\\ \\midrule \n',abstol,d);
+   end
+  fprintf(fid,' \\text{Method} & \\text{MC} & \\text{Lattice} & \\text{Sobol''} & \\text{BayesLat} & \\text{BayesSobol}  \\\\\n');
   fprintf(fid,' \\text{Absolute Error} & \\num{%3.2e} & \\num{%3.2e} & \\num{%3.2e}  & \\num{%3.2e}  & \\num{%3.2e}  \\\\\n', ...
      round([avgAbsErrMC(i); avgAbsErrLat(i); avgAbsErrSob(i); avgAbsErrBayLat(i); avgAbsErrBaySob(i)], 2, 'significant'));
   fprintf(fid,' \\text{Tolerance Met} & \\num{%5.0f} \\%% & \\num{%5.0f} \\%% & \\num{%5.0f} \\%% & \\num{%5.0f} \\%% & \\num{%5.0f} \\%%  \\\\\n', ...
@@ -27,8 +31,10 @@ for i = 1:length(avgAbsErrMC)
      round([nSampleMC(i); nSampleLat(i); nSampleSob(i); nSampleBayLat(i); nSampleBaySob(i)], 2, 'significant'));
   fprintf(fid,' \\text{Time (seconds)} & \\num{%7.4f} & \\num{%7.4f} & \\num{%7.4f} & \\num{%7.4f} & \\num{%7.4f}  \n', ...
      round([timeMC(i); timeLat(i); timeSob(i); timeBayLat(i); timeBaySob(i)], 2,'significant'));
-  if i == 1, fprintf(fid,' \\\\ \\\\'); end
+  if i == 1, fprintf(fid,' \\\\ \\\\ \n'); end
 end
+fprintf(fid,'\\end{array} \n \\]');
+
 fclose(fid);
 
 end
